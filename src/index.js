749c9840450afece719d1ece9b5e307906b6e1a7
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-15 01:09:20 +0300

    Add customer class

--- src/index.js
+++ src/index.js
@@ -7,3 +7,30 @@ class Transaction {
     this.date = date;
   }
 }
+
+class Customer {
+  #name;
+  #id;
+  #transactions;
+
+  constructor(name, id) {
+    this.#name = name;
+    this.#id = id;
+  }
+
+  getName = () => {
+    return this.#name;
+  };
+
+  getId = () => {
+    return this.#id;
+  };
+
+  getTransactions = () => {
+    return this.#transactions;
+  };
+
+  getBalance = () => {};
+
+  addTransactions = (amount) => {};
+}
